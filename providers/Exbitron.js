1e0e549e03a4ad0a9680c45d9ba155c4b4ce35b6
Antonios Papadakis <bdpaboi123@gmail.com>
2023-03-17 14:00:56 -0500

    Exbitron Exchange Base API is partially down, not implementing now

---
+++ providers/Exbitron.js
@@ -0,0 +1,25 @@
+import BaseProvider from "../providers/BaseProvider.js";
+import RequestHelper from "../requestHelper.js";
+
+// TODO finish this later
+//! Exbitron's API is not fully working
+export default class Exbitron extends BaseProvider {
+    constructor(apiSecret, apiKey) {
+        super(apiSecret, apiKey, "https://www.exbitron.com/api/v2/peatio", 0.4, 0.4, 0.01, "Exbitron");
+        this._requestHelper = new RequestHelper({
+            public: {
+                amount: -1,
+                interval: -1
+            }
+        }, true);
+    }
+
+    async initialize() {
+        await this.allTradingPairs();
+        return this;
+    }
+
+    async allTradingPairs() {
+
+    }
+}
