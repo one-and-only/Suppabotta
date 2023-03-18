a053f659685a95c467e6844a880e916c509c3fe8
Antonios Papadakis <bdpaboi123@gmail.com>
2023-03-18 17:27:41 -0500

    Remove Unused Code Block in TickerMaintenance Strategy File

--- strategies/TickerMaintenance.js
+++ strategies/TickerMaintenance.js
@@ -1,7 +1,5 @@
 import BaseStrategy from './BaseStrategy.js';
-// import Logger from '../Logger.js';
 
-// TODO rework so it works on all trading pairs
 export default class TickerMaintenanceStrategy extends BaseStrategy {
     constructor(connectors) {
         super(connectors);
@@ -11,47 +9,11 @@ export default class TickerMaintenanceStrategy extends BaseStrategy {
         this._buyingReferenceCurrency = false;
     }
 
-    // async _balanceCheck() {
-    //     let someNoBalance = false;
-    //     this._lastBalanceCheckTimestamp = Date.now();
-    //     for (const connector of this._connectors) {
-    //         // console.log(connector._tradingPairs);
-    //         const rtmBalance = await connector.getBalance("rtm");
-
-    //         // get balances for each trading pair
-    //         for (const key in connector._tradingPairs) {
-    //             const pair = connector._tradingPairs[key];
-
-    //             const minimumRequired = connector.minOrderSize(key);
-
-    //             const coin1Balance = await connector.getBalance(key);
-    //             console.log(`${key}: ${coin1Balance}`);
-    //             if (coin1Balance.available < minimumRequired || rtmBalance < minimumRequired) {
-    //                 connector._tradingPairs[key].enabled = false;
-    //                 someNoBalance = true;
-    //             }
-    //         }
-    //     }
-
-    //     if (someNoBalance) {
-    //         Logger.warning("TickerMaintenance", "balanceCheck", "Some connectors and/or trading pairs don't have enough balance for trading. Ticker maintenance for those connectors will be disabled until there is enough balance");
-    //     }
-    // }
-
-    async start() {
-        // await this._balanceCheck();
-    }
+    async start() { }
 
     async tick() {
         console.log("tick");
         const currentTimestamp = Date.now();
-
-        // // recheck balance just in case the user traded outside the app
-        // if (currentTimestamp - this._lastBalanceCheckTimestamp > 60000) {
-        //     console.log("Balance check");
-        //     this._lastBalanceCheckTimestamp = currentTimestamp;
-        //     await this._balanceCheck();
-        // }
 
         // ticker maintenance every 5 minutes
         if (currentTimestamp - this._lastMaintainedTimestamp > 300000) {
