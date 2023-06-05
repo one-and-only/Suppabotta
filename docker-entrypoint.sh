3742f093ab9c93177fb60a44a426c686a9cd45c5
Antonios Papadakis <bdpaboi123@gmail.com>
2023-06-05 16:46:52 -0500

    No Idea Why They Show Up As Changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-cd app
-npm i
+cd app || exit 2
+
+if [ ! -d "/app" ]; then
+    echo "Please link the app directory to the docker container (\`-v ...:...\`)"
+    return
+fi
+
+if [ ! -d "node_modules" ]; then
+    npm i
+fi
+
 node index.js
