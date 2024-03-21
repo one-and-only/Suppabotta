fa18c7e52798d4c7a1891e16546b38c27f07d92e
Antonios Papadakis <bdpaboi123@gmail.com>
2024-03-21 11:26:52 -0400

    `BaseStrategy` Now Supports Custom Outbound IP

--- strategies/BaseStrategy.js
+++ strategies/BaseStrategy.js
@@ -5,6 +5,7 @@ export default class BaseStrategy {
     _socketBroadcaster;
     _paperTradingMongoCollection;
     _recentPaperTrades;
+    _outboundIp;
 
     /**
      * 
@@ -15,6 +16,8 @@ export default class BaseStrategy {
         this._socketBroadcaster = args.socketBroadcaster;
         this._paperTradingMongoCollection = paperTradingMongoCollection;
         this._recentPaperTrades = [];
+        console.assert(args.ip, "Outbound IP Address was not correctly passed in as an argument internally or trading parameter was set incorrectly");
+        this._outboundIp = args.ip;
     }
 
     async start() {
